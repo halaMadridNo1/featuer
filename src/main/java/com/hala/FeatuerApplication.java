c0168e817d6872e517143993f184c6b51512e18f
halaMadridNo1 <18582995995@163.com>
2022-10-19 14:40:21 +0800

    Initial commit

---
+++ src/main/java/com/hala/FeatuerApplication.java
@@ -0,0 +1,13 @@
+package com.hala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatuerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatuerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hala/FeatuerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatuerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
